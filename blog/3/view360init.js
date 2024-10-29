d9dbe00c4149be4a71d83ae709117c545355faac
baulus <luix.herzog@proton.me>
2024-10-29 15:22:44 +0800

    fixed paths and created sample page for 5th and 6th blog article, also translated everything into german

--- blog/3/view360init.js
+++ blog/3/view360init.js
@@ -1,18 +1,57 @@
 
 const viewer = new View360("#viewer", {
     projection: new View360.EquirectProjection({
-      src: "/static/360/R0010750.JPG",
+      src: "pictures/360/R0010753.JPG",
       video: false
     })
   });
 
-  
-  const viewer2 = new View360("#viewer2", {
-    projection: new View360.EquirectProjection({
-      src: "/static/360/R0010815_st.MP4",
-      video: {
-        autoplay: true,
-        loop: true
-      }
-    })
-  });
+const viewer2 = new View360("#viewer2", {
+  projection: new View360.EquirectProjection({
+    src: "pictures/360/R0010763.JPG",
+    video: false
+  })
+});
+
+const viewer3 = new View360("#viewer3", {
+  projection: new View360.EquirectProjection({
+    src: "pictures/360/R0010776.JPG",
+    video: false
+  })
+});
+
+const viewer4 = new View360("#viewer4", {
+  projection: new View360.EquirectProjection({
+    src: "pictures/360/R0010780.JPG",
+    video: false
+  })
+});
+
+const viewer5 = new View360("#viewer5", {
+  projection: new View360.EquirectProjection({
+    src: "pictures/360/R0010785.JPG",
+    video: false
+  })
+});
+
+const viewer6 = new View360("#viewer6", {
+  projection: new View360.EquirectProjection({
+    src: "pictures/360/R0010811.JPG",
+    video: false
+  })
+});
+
+const viewer7 = new View360("#viewer7", {
+  projection: new View360.EquirectProjection({
+    src: "pictures/360/R0010820.JPG",
+    video: false
+  })
+});
+
+
+const viewer8 = new View360("#viewer8", {
+  projection: new View360.EquirectProjection({
+    src: "pictures/360/R0010822.JPG",
+    video: false
+  })
+});
